50959f2f576deddd2244a83533e17bcb092a3d86
moonseunghoo <tmdgn2002@gmail.com>
2021-02-18 23:48:14 +0900

    재무분석 크롤링 (수정 O)

--- thrid_page_crawler.py
+++ thrid_page_crawler.py
@@ -1,22 +1,10 @@
-import math
-
-import requests # 웹 페이지 소스를 얻기 위한 패키지(기본 내장 패키지이다.)
-import pandas as pd # 데이터를 처리하기 위한 가장 기본적인 패키지
 import time # 사이트를 불러올 때, 작업 지연시간을 지정해주기 위한 패키지이다. (사이트가 늦게 켜지면 에러가 발생하기 때문)
-import urllib.request
-import json
-import re
-import datetime as dt
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
+import traceback
 from bs4 import BeautifulSoup # 웹 페이지 소스를 얻기 위한 패키지, 더 간단히 얻을 수 있다는 장점이 있다고 한다.
-from datetime import datetime
-from pykrx import stock
 from selenium import webdriver
-from selenium.webdriver import Chrome
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from ticker_name_crawler import ticker
+
+start = time.time()
 
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
@@ -29,6 +17,7 @@ delay = 5
 
 def Financial_Analysis(code):
     name = code
+    global GM
     base_url = "https://finance.naver.com/item/coinfo.nhn?code=" + name + "&target=finsum_more"
 
     browser_thrid_p.get(base_url)
@@ -43,7 +32,7 @@ def Financial_Analysis(code):
     browser_thrid_p.implicitly_wait(delay)
     browser_thrid_p.find_element_by_xpath('//*[@id="hfinGubun"]').click()
     browser_thrid_p.implicitly_wait(delay)
-    time.sleep(0.5)
+    time.sleep(0.25)
 
     htmlFA = browser_thrid_p.page_source
     htmlFA0 = BeautifulSoup(htmlFA, 'html.parser')
@@ -64,7 +53,7 @@ def Financial_Analysis(code):
 
         GM0 = []
         for i in range(2, 6):  # 값 입력
-            GM0.append(tdGM[i].text)
+            GM0.append(tdGM[i].text.strip())
 
         for i in range(len(GM0)):  # 값 ,제거
             GM0[i] = GM0[i].replace(',', '')
@@ -72,8 +61,6 @@ def Financial_Analysis(code):
         for i in range(len(GM0)):  # 공백 제거
             if GM0[i] == '':
                 GM0[i] = '0'
-            else:
-                break
 
         strGM = list(map(float, GM0))
         GM = sum(strGM)
@@ -81,10 +68,30 @@ def Financial_Analysis(code):
 
     return GM
 
-code = "000020"
+try:
+    for code in ticker:
+        GM = Financial_Analysis(code)
+        print(code)
+        print(GM)
 
-GM = Financial_Analysis(code)
-
-# print(GM)
+except Exception as e:
+    browser_thrid_p.quit()
+    print(traceback.format_exc())
+    print(e)
 
 browser_thrid_p.quit()
+
+print(time.time()-start)
+
+# code = "008060"
+# try:
+#     GM = Financial_Analysis(code)
+#     print(code)
+#     print(GM)
+# except Exception as e:
+#     browser_thrid_p.quit()
+#     print(traceback.format_exc())
+#     print(e)
+# browser_thrid_p.quit()
+#
+# print(time.time()-start)
