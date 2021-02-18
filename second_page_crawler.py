aba1257ab45b6ce10dc9771805a84d95e14d1dc9
moonseunghoo <tmdgn2002@gmail.com>
2021-02-18 20:48:55 +0900

    기업개요 크롤링 (수정 O)

--- second_page_crawler.py
+++ second_page_crawler.py
@@ -1,21 +1,8 @@
-import math
-import requests # 웹 페이지 소스를 얻기 위한 패키지(기본 내장 패키지이다.)
-import pandas as pd # 데이터를 처리하기 위한 가장 기본적인 패키지
-import time # 사이트를 불러올 때, 작업 지연시간을 지정해주기 위한 패키지이다. (사이트가 늦게 켜지면 에러가 발생하기 때문)
-import urllib.request
-import json
 import re
-import datetime as dt
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
+import traceback
 from bs4 import BeautifulSoup # 웹 페이지 소스를 얻기 위한 패키지, 더 간단히 얻을 수 있다는 장점이 있다고 한다.
-from datetime import datetime
-from pykrx import stock
 from selenium import webdriver
-from selenium.webdriver import Chrome
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from ticker_name_crawler import ticker
 
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
@@ -55,10 +42,30 @@ def Enterprise_Overview(code):
 
     return IPODate
 
-code = "000020"
 
-IPODate = Enterprise_Overview(code)
 
-# print(IPODate)
+
+
+
+
+try:
+    for code in ticker:
+        IPODate = Enterprise_Overview(code)
+        print(code)
+        print(IPODate)
+except Exception as e:
+    browser_second_p.quit()
+    print(traceback.format_exc())
+    print(e)
+
+# code = "000020"
+# try:
+#     IPODate = Enterprise_Overview(code)
+#     print(IPODate)
+#
+# except Exception as e:
+#     browser_second_p.quit()
+#     print(traceback.format_exc())
+#     print(e)
 
 browser_second_p.quit()
