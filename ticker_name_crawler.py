8a05617eaf4d54cf4d49e5bf0bfdc1a833023eb8
moonseunghoo <tmdgn2002@gmail.com>
2021-02-18 18:42:59 +0900

    티커,종목명 크롤링 (수정 O,KOSPI)

--- ticker_name_crawler.py
+++ ticker_name_crawler.py
@@ -1,7 +1,7 @@
 from pykrx import stock
 
 def All_stock():
-    df = stock.get_market_ohlcv_by_ticker('20210214',market='ALL')
+    df = stock.get_market_ohlcv_by_ticker('20210217',market='KOSPI')
     list_of_index = [item for item in df.index]
     ticker = sorted(list_of_index) #종목코드 오름차순
     ticker_name = [stock.get_market_ticker_name(i) for i in ticker] # 종목명 오름차순
